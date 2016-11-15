2decb4e0816bc98e31d4706aa30ea775a2783cbb
Lukas Moser <lukas.moser@iet-gibb.ch>
2016-11-15 11:22:12 +0100

    WebView with Materialize css

--- src/main/java/ch/gibb/iet/modul306/vmlauncher/view/AbstractView.java
+++ src/main/java/ch/gibb/iet/modul306/vmlauncher/view/AbstractView.java
@@ -1,5 +1,20 @@
 package ch.gibb.iet.modul306.vmlauncher.view;
 
-public abstract class AbstractView {
+import javax.swing.JFrame;
 
+public abstract class AbstractView {
+	
+	private static JFrame frame;
+	private static String title = "VM Launcher";
+	
+	public AbstractView(){
+		
+		initFrame();
+	}
+	private static void initFrame(){
+		
+		frame = new JFrame(title);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+	}
 }
