7554771613744cc0c3fdb843eaf5fc5db6ae86f6
Timon Borter <timon.borter@gmx.ch>
2016-11-26 14:38:59 +0100

    nearly finished backup modul.. missing "finished"-message

--- src/main/java/ch/gibb/iet/modul306/vmlauncher/controller/BackupController.java
+++ src/main/java/ch/gibb/iet/modul306/vmlauncher/controller/BackupController.java
@@ -12,6 +12,8 @@ import ch.gibb.iet.modul306.vmlauncher.model.BackupModel;
 import ch.gibb.iet.modul306.vmlauncher.model.MachineModel;
 import ch.gibb.iet.modul306.vmlauncher.model.objects.XMLMachine;
 import ch.gibb.iet.modul306.vmlauncher.view.BackupView;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -23,9 +25,7 @@ public class BackupController extends AbstractController {
 	@Autowired
 	private MachineModel machineModel;
 
-	public BackupController() {
-		super();
-	}
+	private BackupModel backupModel;
 
 	@Override
 	public void loadView(Stage mainStage) {
@@ -46,7 +46,38 @@ public class BackupController extends AbstractController {
 
 		File destination = directoryChooser.showDialog(mainStage);
 
-		new BackupModel(this).backupMachine(machine, destination, null);
+		if (backupModel == null) {
+			backupModel = new BackupModel(this);
+		}
+
+		Alert information = new Alert(AlertType.INFORMATION);
+		information.setTitle("Backup");
+		information.setContentText(
+				"Backup runs in asynchronous task. You can still continue to browse your other machines. You will be informed if the task completed.");
+		information.show();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					backupModel.backupMachine(machine, destination, null);
+				} catch (ZipException e) {
+					Alert error = new Alert(AlertType.ERROR);
+					error.setTitle("Error");
+					error.setHeaderText("An error occured while backing up " + machine.name + ".");
+					error.setContentText(e.getLocalizedMessage());
+					error.show();
+
+					LOGGER.fatal(e.getLocalizedMessage());
+				}
+
+				// TODO: Fix "this is not an fx-thread"
+				Alert done = new Alert(AlertType.INFORMATION);
+				done.setTitle("Finished backup");
+				done.setContentText("Machine " + machine.name + " successfully backed up.");
+				done.show();
+			}
+		}).start();
 	}
 
 	public void restoreMachine(XMLMachine machine, Stage mainStage) throws ZipException {
@@ -56,18 +87,49 @@ public class BackupController extends AbstractController {
 
 		File source = fileChooser.showOpenDialog(mainStage);
 
-		Arrays.asList(new File(machine.path).listFiles()).forEach(subFile -> {
-			if (subFile.isDirectory()) {
+		if (backupModel == null) {
+			backupModel = new BackupModel(this);
+		}
+
+		Alert information = new Alert(AlertType.INFORMATION);
+		information.setTitle("Backup");
+		information.setContentText(
+				"Restore runs in asynchronous task. You can still continue to browse your other machines. You will be informed if the task completed.");
+		information.show();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Arrays.asList(new File(machine.path).listFiles()).forEach(subFile -> {
+					if (subFile.isDirectory()) {
+						try {
+							FileUtils.deleteDirectory(subFile);
+						} catch (IOException e) {
+							Alert error = new Alert(AlertType.ERROR);
+							error.setTitle("Error");
+							error.setHeaderText("An error occured while restoring up " + machine.name + ".");
+							error.setContentText(e.getLocalizedMessage());
+							error.show();
+
+							LOGGER.fatal(e.getLocalizedMessage());
+						}
+					} else {
+						subFile.delete();
+					}
+				});
+
 				try {
-					FileUtils.deleteDirectory(subFile);
-				} catch (IOException e) {
+					backupModel.uncompressFile(source.getAbsolutePath(), machine.path, null);
+				} catch (ZipException e) {
 					LOGGER.fatal(e.getLocalizedMessage());
 				}
-			} else {
-				subFile.delete();
-			}
-		});
 
-		new BackupModel(this).uncompressFile(source.getAbsolutePath(), machine.path, null);
+				// TODO: Fix "this is not an fx-thread"
+				Alert done = new Alert(AlertType.INFORMATION);
+				done.setTitle("Finished backup");
+				done.setContentText("Machine " + machine.name + " successfully backed up.");
+				done.show();
+			}
+		}).start();
 	}
 }
